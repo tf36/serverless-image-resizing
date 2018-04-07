27780e781325726604bfa9035bd33cedb5ffec6d
Tyrone <tferguson036@gmail.com>
2018-04-06 22:52:54 -0400

    allow nullable width/height; embed image

--- lambda/index.js
+++ lambda/index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const AWS = require('aws-sdk');
 const S3 = new AWS.S3({
   signatureVersion: 'v4',
@@ -8,48 +6,57 @@ const Sharp = require('sharp');
 
 const BUCKET = process.env.BUCKET;
 const URL = process.env.URL;
-const ALLOWED_DIMENSIONS = new Set();
-
-if (process.env.ALLOWED_DIMENSIONS) {
-  const dimensions = process.env.ALLOWED_DIMENSIONS.split(/\s*,\s*/);
-  dimensions.forEach((dimension) => ALLOWED_DIMENSIONS.add(dimension));
-}
+const ALLOWED_RESOLUTIONS = process.env.ALLOWED_RESOLUTIONS ? new Set(process.env.ALLOWED_RESOLUTIONS.split(/\s*,\s*/)) : new Set([]);
 
 exports.handler = function(event, context, callback) {
-  const key = event.queryStringParameters.key;
-  const match = key.match(/((\d+)x(\d+))\/(.*)/);
-  const dimensions = match[1];
-  const width = parseInt(match[2], 10);
-  const height = parseInt(match[3], 10);
-  const originalKey = match[4];
+    const key = event.queryStringParameters.key;
+    const match = key.match(/((\d*)x(\d*))\/(.*)/);
 
-  if(ALLOWED_DIMENSIONS.size > 0 && !ALLOWED_DIMENSIONS.has(dimensions)) {
-     callback(null, {
-      statusCode: '403',
-      headers: {},
-      body: '',
-    });
-    return;
-  }
+    //Check if requested resolution is allowed
+    if(0 != ALLOWED_RESOLUTIONS.size && !ALLOWED_RESOLUTIONS.has(match[1]) ) {
+        callback(null, {
+            statusCode: '403',
+            headers: {},
+            body: '',
+        });
 
-  S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
-    .then(data => Sharp(data.Body)
-      .resize(width, height)
-      .toFormat('png')
-      .toBuffer()
-    )
-    .then(buffer => S3.putObject({
-        Body: buffer,
-        Bucket: BUCKET,
-        ContentType: 'image/png',
-        Key: key,
-      }).promise()
-    )
-    .then(() => callback(null, {
-        statusCode: '301',
-        headers: {'location': `${URL}/${key}`},
-        body: '',
-      })
-    )
-    .catch(err => callback(err))
+        return;
+    }
+
+    const width = (match[2] && !isNaN(match[2]) && parseInt(match[2], 10)) || null;
+    const height = (match[3] && !isNaN(match[3]) && parseInt(match[3], 10)) || null;
+    const originalKey = match[4];
+
+    //Simple check if proper arguments given
+    if (width || height) {
+        callback(null, {
+            statusCode: '403',
+            headers: {},
+            body: '',
+        });
+
+        return;
+    }
+
+    S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
+        .then(data => Sharp(data.Body)
+            .resize(width, height)
+            .embed()
+            .toBuffer()
+        )
+        .then(buffer => S3.putObject({
+                Body: buffer,
+                Bucket: BUCKET,
+                ContentType: 'image/png',
+                Key: key,
+            }).promise()
+        )
+        .then(() => callback(null, {
+                statusCode: '301',
+                headers: {'location': `${URL}/${key}`},
+                body: '',
+            })
+        )
+        .catch(err => callback(err))
 }
+
